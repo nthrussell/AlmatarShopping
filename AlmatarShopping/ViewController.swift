db40f3a195427eef61f6d8f2d9d82a36f1a19cd2
M A Russel <arafin@lastmanstands.com>
2025-08-05 13:53:03 +0600

    Initial Commit

---
+++ AlmatarShopping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlmatarShopping
+//
+//  Created by Minhajul Arafin Russel on 5/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AlmatarShoppingTests/AlmatarShoppingTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlmatarShoppingTests.swift
+//  AlmatarShoppingTests
+//
+//  Created by Minhajul Arafin Russel on 5/8/25.
+//
+
+import Testing
+@testable import AlmatarShopping
+
+struct AlmatarShoppingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
