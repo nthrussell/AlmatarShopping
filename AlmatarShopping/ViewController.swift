4080f5e388564718c4526c0b53efb59ab0d6c242
M A Russel <rredxx@icloud.com>
2025-08-06 21:54:33 +0600

    Host app to test ALSLFramework

--- AlmatarShopping/ViewController.swift
+++ AlmatarShopping/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import ALSLFramework
 
 class ViewController: UIViewController {
-
+    
+    private(set) lazy var SLButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 12
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationController?.navigationBar.topItem?.title = "Almatar Shopping"
+        
+        view.backgroundColor = .white
+        view.addSubview(SLButton)
+        setUpLayouts()
+        
+        SLButton.setTitle("Go to Shopping List", for: .normal)
     }
-
-
+    
+    @objc
+    func tapAction() {
+        print("button tapped")
+        let vc: UIViewController = ShoppingListFactory.makeViewController() as! UIViewController
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func setUpLayouts() {
+        NSLayoutConstraint.activate([
+            SLButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            SLButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            SLButton.heightAnchor.constraint(equalToConstant: 50),
+            SLButton.widthAnchor.constraint(equalToConstant: 200),
+        ])
+    }
 }
 
